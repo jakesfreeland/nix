7976c5271faa103e393b174238d8546e70611cdc
Jake Freeland <jfree@FreeBSD.org>
2026-01-21 11:53:33 -0600

    sys/timerfd: Allow use on FreeBSD
    FreeBSD has natively supported timerfd(2) system calls since FreeBSD
    14. Add missing cfg directives for FreeBSD builds so timerfd is
    accessible.

--- src/sys/time.rs
+++ src/sys/time.rs
@@ -13,7 +13,10 @@ const fn zero_init_timespec() -> timespec {
 }
 
 #[cfg(any(
-    all(feature = "time", any(target_os = "android", target_os = "linux")),
+    all(
+        feature = "time",
+        any(target_os = "android", target_os = "freebsd", target_os = "linux")
+    ),
     all(
         any(
             target_os = "freebsd",
